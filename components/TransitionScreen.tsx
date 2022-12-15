3d1932a2ebb8bac3dbecc7461586fdef985f5c5e
tuanddd <vincenttuandao@gmail.com>
2022-12-15 14:38:13 +0700

    chore: welcome screen + transition

---
+++ components/TransitionScreen.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const fadeIn = [
+  {
+    borderWidth: "0vw",
+  },
+  {
+    borderWidth: "50vw",
+  },
+];
+
+type Props = {
+  children: (anim: Animation) => React.ReactNode;
+};
+
+export const TransitionScreen = ({ children }: Props) => {
+  const [anim, setAnim] = useState<Animation>();
+  const transitionRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (transitionRef.current) {
+      const animation = transitionRef.current.animate(fadeIn, {
+        duration: 700,
+        fill: "forwards",
+      });
+      animation.pause();
+      setAnim(animation);
+    }
+  }, [transitionRef]);
+
+  return (
+    <div className="w-screen h-screen">
+      {anim ? children(anim) : null}
+      <div
+        ref={transitionRef}
+        className="z-10 pointer-events-none border-gray-900 fixed scale-[2] min-h-screen w-screen rounded-full aspect-square left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -mt-96"
+      />
+    </div>
+  );
+};
